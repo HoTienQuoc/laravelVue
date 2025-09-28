a69a51ad66655973d8d18979abc5113f26f9b140
HoTienQuoc <hotienquoc0429@gmail.com>
2025-09-28 14:21:09 +0700

    Add Bootstrap for styling, integrate Axios for API calls, and restructure components

---
+++ consuming-rest-api/src/components/tasks/Task.vue
@@ -0,0 +1,45 @@
+<template>
+    <li class="list-group-item py-3">
+        <div
+            class="d-flex justify-content-start align-items-center"
+        >
+            <input
+                class="form-check-input mt-0 completed"
+                type="checkbox"
+            />
+            <div
+                class="ms-2 flex-grow-1"
+                title="Double click the text to edit or remove"
+            >
+                <!-- <div class="relative">
+                    <input class="editable-task" type="text" />
+                </div> -->
+                <span>{{ task.name }}</span>
+            </div>
+            <!-- <div class="task-date">24 Feb 12:00</div> -->
+        </div>
+        <div class="task-actions">
+            <button
+                class="btn btn-sm btn-circle btn-outline-secondary me-1"
+            >
+                <icons-pencils/>
+            </button>
+            <button
+                class="btn btn-sm btn-circle btn-outline-danger"
+            >
+                <IconTrash/>
+            </button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+import IconsPencils from '../icons/IconsPencils.vue';
+import IconTrash from '../icons/IconTrash.vue';
+
+defineProps({
+    task: Object,
+});
+
+
+</script>
